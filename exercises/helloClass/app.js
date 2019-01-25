c586347bbf67a6beb6e6c02cb3200535ccc2db07
Nicole Cedano <cedano.nicolee@gmail.com>
2019-01-24 17:02:04 -0700

    hey

---
+++ exercises/helloClass/app.js
@@ -0,0 +1,15 @@
+// //Function Expression
+// var sayHello =function(){
+//     console.log("Hello Class")
+// }
+
+var person = "Mario"
+    //argument
+    //parameter
+
+//Function Declaration
+function sayHello (name) {
+    console.log("Hello " + name)
+}
+sayHello("Mario")
+
