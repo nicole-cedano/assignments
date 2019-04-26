f90a1e823fd418ab771828c3a2782f193c2e1f70
Nicole Cedano <cedano.nicolee@gmail.com>
2019-04-26 02:37:56 -0600

    making changes to portfolio

--- projects/personal-portfolio/client/src/components/Projects.js
+++ projects/personal-portfolio/client/src/components/Projects.js
@@ -2,29 +2,43 @@ import React from 'react'
 import Juan from "./pics/Juan.png"
 import Park from './pics/Park.png'
 import api from './pics/api.png'
+import Todo from './pics/Todo.png'
+import Zen from './pics/Zen.png'
 
 const Projects = () => {
     return (
-        <div className="projects">
-            <span className="project-card" >
-                <img src={Juan} />
-                Juan's Photography
-                <a href="https://github.com/nicole-cedano/juan-photography">View Git</a>
-        <div className="project-overlay"></div>
-            </span>
 
-            <span className="project-card" >
-                <img src={Park} />
-                Parking U
-        <div className="project-overlay"></div>
-        <a href="https://parkingu.herokuapp.com/">View App</a>
-            </span>
-            <span className="project-card">
-                <img src={api} />
-                Space Mumbo Jumbo
+        <div className="projects">
+            <div className="gradient">
+                <span className="project-card" style={{ background: `url(${Juan})`, backgroundSize: "cover", backgroundAttachment: "scroll", backgroundPosition: "center", backgroundRepeat: "no-repeat" }}>
+                    Juan's Photography
+                <a href="https://github.com/nicole-cedano/juan-photography">View Git</a>
+                </span>
+            </div>
+            <div className="gradient">
+                <span className="project-card" style={{ background: `url(${Park})`, backgroundSize: "cover", backgroundAttachment: "scroll", backgroundPosition: "center", backgroundRepeat: "no-repeat" }}>
+                    Parking U
+                    <a href="gradient">View App</a>
+                </span>
+            </div>
+            <div className="gradient">
+                <span className="project-card" style={{ background: `url(${api})`, backgroundSize: "cover", backgroundAttachment: "scroll", backgroundPosition: "center", backgroundRepeat: "no-repeat" }}>
+                    Space Mumbo Jumbo
                 <a href="http://spacemumbojumbo.surge.sh/">View App</a>
-        <div className="project-overlay"></div>
-            </span>
+                </span>
+            </div>
+            <div className="gradient">
+                <span className="project-card" style={{ background: `url(${Todo})`, backgroundSize: "cover", backgroundAttachment: "scroll", backgroundPosition: "center", backgroundRepeat: "no-repeat" }}>
+                    Todo List
+                <a href="http://todoaxiosnc.surge.sh/">View App</a>     
+                </span>
+            </div>
+            <div className="gradient">
+                <span className="project-card" style={{ background: `url(${Zen})`, backgroundSize: "cover", backgroundAttachment: "scroll", backgroundPosition: "center", backgroundRepeat: "no-repeat" }}>
+                    Css Zen Garden
+                <a href="http://csszengardennc.surge.sh/">View App</a>
+                </span>
+            </div>
         </div>
     )
 }
--- projects/personal-portfolio/client/src/components/Resume.js
+++ projects/personal-portfolio/client/src/components/Resume.js
@@ -1,38 +1,50 @@
 import React, { Component } from 'react'
-import { Document, Page} from 'react-pdf'
-import resume from "./NCresume.pdf"
-import { pdfjs } from 'react-pdf';
-pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.js`;
+// import { Document, Page} from 'react-pdf'
+// import resume from "./NCresume.pdf"
+// import { pdfjs } from 'react-pdf';
+import backup from "./NCresume.jpg"
+// pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.js`;
 
 
-class Resume extends Component {
-    constructor() {
-        super()
-        this.state = {
-            numPages: null,
-            pageNumber: 1,
-        }
-    }
-    // loading pdf
-    onDocumentLoadSuccess = ({ numPages }) => {
-        this.setState({ numPages })
-    }
-    render() {
-        const { pageNumber} = this.state;
-        return (
-            <div className='resume'>
-                <Document
-                options={{
-                cMapUrl: 'cmaps/',
-                cMapPacked: true}}
-                    className="pdfdoc"
-                    file={resume}
-                    onLoadSuccess={this.onDocumentLoadSuccess}>
-                    <Page  className= "page" scale="1.75" pageNumber={pageNumber} />
-                </Document>
-            </div>
-        )
-    }
+// class Resume extends Component {
+//     constructor() {
+//         super()
+//         this.state = {
+//             numPages: null,
+//             pageNumber: 1,
+//         }
+//     }
+//     // loading pdf
+//     onDocumentLoadSuccess = ({ numPages }) => {
+//         this.setState({ numPages })
+//     }
+
+//     render() {
+//         const { pageNumber} = this.state;
+//         return (
+//             <div className='resume'>
+//                 <Document
+//                 noData= {<div>{backup}</div>}
+//                 options={{
+//                 workerSrc: "/pdf.worker.js",
+//                 cMapUrl: 'cmaps/',
+//                 cMapPacked: true}}
+//                     className="pdfdoc"
+//                     file={resume}
+//                     onLoadSuccess={this.onDocumentLoadSuccess}>
+//                     <Page  className= "page" scale="1.75" pageNumber={pageNumber} />
+//                 </Document>
+//             </div>
+//         )
+//     }
+// }
+
+const Resume = () => {
+    return (
+        <div className="resume">
+        <img  className="resume-backup" src={backup} />
+        </div>
+    )
 }
 
 export default Resume
